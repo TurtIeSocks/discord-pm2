9794bc15faadddca2022d5a38a30ba4757d1fb70
Derick M <58572875+TurtIeSocks@users.noreply.github.com>
2023-06-18 18:19:17 -0400

    cleanup interaction handler

--- src/discord/events/interaction.ts
+++ src/discord/events/interaction.ts
@@ -4,62 +4,45 @@ import { HELPERS, log } from '../../services/logger'
 
 export function interaction(client: Client): void {
   client.on(Events.InteractionCreate, async (interaction: Interaction) => {
-    if (interaction.isCommand()) {
-      const command = interaction.client.ctx.commands.get(
-        interaction.commandName,
-      )
-      if (!command) {
-        interaction.followUp(
-          `An error has occurred with input: ${interaction.commandName}`,
-        )
-        return
-      }
+    const isModalSubmit = interaction.isModalSubmit()
+    const isAutocomplete = interaction.isAutocomplete()
+    const isCommand = interaction.isCommand()
+    const name = isModalSubmit
+      ? interaction.customId
+      : isAutocomplete || isCommand
+      ? interaction.commandName
+      : 'Unknown'
 
-      log.info(
-        HELPERS.discord,
-        interaction.commandName,
-        interaction.user.tag,
-        interaction.guild?.name ?? interaction.guildId ?? 'DM',
-      )
-      await command.run(interaction)
-    } else if (interaction.isModalSubmit()) {
-      const command = interaction.client.ctx.commands.get(interaction.customId)
-      if (!command) {
-        interaction.followUp(
-          `An error has occurred with input: ${interaction.customId}`,
-        )
-        return
-      }
-
-      log.info(
-        HELPERS.discord,
-        interaction.customId,
-        interaction.user.tag,
-        interaction.guild?.name ?? interaction.guildId ?? 'DM',
-      )
-      if (command.modal) await command.modal(interaction)
-    } else if (interaction.isAutocomplete()) {
-      const command = interaction.client.ctx.commands.get(
-        interaction.commandName,
-      )
-      if (!command) {
-        log.error(
-          HELPERS.discord,
-          `No command matching ${interaction.commandName} was found.`,
-        )
-        return
-      }
-
-      try {
+    try {
+      if (isCommand) {
+        const command = interaction.client.ctx.commands.get(name)
+        if (!command) throw new Error(`Command not found`)
+        await command.run(interaction)
+      } else if (isModalSubmit) {
+        const command = interaction.client.ctx.commands.get(name)
+        if (!command) throw new Error(`Modal submit not found`)
+        if (command.modal) await command.modal(interaction)
+      } else if (isAutocomplete) {
+        const command = interaction.client.ctx.commands.get(name)
+        if (!command) throw new Error(`Autocomplete not found`)
         if (command.autoComplete) await command.autoComplete(interaction)
-      } catch (error) {
-        log.error(
-          HELPERS.discord,
-          interaction.commandName,
-          interaction.user.tag,
-          interaction.guild?.name ?? interaction.guildId ?? 'DM',
-          error,
-        )
+      }
+      log.info(
+        HELPERS.discord,
+        name,
+        interaction.user.tag,
+        interaction.guild?.name ?? interaction.guildId ?? 'DM',
+      )
+    } catch (error) {
+      log.error(
+        HELPERS.discord,
+        name,
+        interaction.user.tag,
+        interaction.guild?.name ?? interaction.guildId ?? 'DM',
+        error,
+      )
+      if (isModalSubmit || isCommand) {
+        await interaction.followUp(`An error has occurred with input: ${name}`)
       }
     }
   })
